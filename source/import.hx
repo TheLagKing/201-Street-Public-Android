9f286777bbb7154476295770fa1e488d241004d7
TheLagKing <dalucanogheaven12@gmail.com>
2025-12-01 08:58:35 +0800

    Add Android-specific imports in import.hx

--- source/import.hx
+++ source/import.hx
@@ -15,6 +15,24 @@ import flixel.FlxBasic;
 import flixel.math.FlxPoint;
 import flixel.sound.FlxSound;
 
+import mobile.flixel.FlxHitbox;
+import mobile.flixel.FlxVirtualPad;
+import mobile.flixel.input.FlxMobileInputID;
+import mobile.backend.SUtil;
+
+//Android
+#if android
+import android.content.Context as AndroidContext;
+import android.widget.Toast as AndroidToast;
+import android.os.Environment as AndroidEnvironment;
+import android.Permissions as AndroidPermissions;
+import android.Settings as AndroidSettings;
+import android.Tools as AndroidTools;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+import android.os.BatteryManager as AndroidBatteryManager;
+#end
+
 #if sys
 import sys.*;
 
