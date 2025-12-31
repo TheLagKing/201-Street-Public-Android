7d1b926bc8afdf3fe42cd479f7b1607223436130
CybrrPortGames <dalucanogheaven12@gmail.com>
2026-01-01 05:36:46 +0800

    Update OverworldEditorState.hx

--- source/funkin/states/editors/OverworldEditorState.hx
+++ source/funkin/states/editors/OverworldEditorState.hx
@@ -230,7 +230,9 @@ class OverworldEditorState extends MusicBeatState
 		var data:String = Json.stringify(mapData, "\t");
 		if (data.length > 0)
 		{
+#if desktop
 			_file = new FileReference();
+#end
 			_file.addEventListener(Event.COMPLETE, onSaveComplete);
 			_file.addEventListener(Event.CANCEL, onSaveCancel);
 			_file.addEventListener(IOErrorEvent.IO_ERROR, onSaveError);
