ab6509f05f665ab3f2552399d25324dd4f2b0e01
CybrrPortGames <dalucanogheaven12@gmail.com>
2026-01-02 04:01:13 +0800

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -26,12 +26,12 @@ import flixel.util.FlxDestroyUtil;
 import android.content.Context as AndroidContext;
 import android.widget.Toast as AndroidToast;
 import android.os.Environment as AndroidEnvironment;
-import android.Permissions as AndroidPermissions;
-import android.Settings as AndroidSettings;
-import android.Tools as AndroidTools;
-import android.os.Build.VERSION as AndroidVersion;
-import android.os.Build.VERSION_CODES as AndroidVersionCode;
-import android.os.BatteryManager as AndroidBatteryManager;
+//import android.Permissions as AndroidPermissions;
+//import android.Settings as AndroidSettings;
+//import android.Tools as AndroidTools;
+//import android.os.Build.VERSION as AndroidVersion;
+//import android.os.Build.VERSION_CODES as AndroidVersionCode;
+//import android.os.BatteryManager as AndroidBatteryManager;
 #end
 
 #if sys
