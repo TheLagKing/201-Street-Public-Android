3fccbadac74660543bf1b439a3cc1e18acd80426
CybrrPortGames <dalucanogheaven12@gmail.com>
2026-01-03 18:07:55 +0800

    Update import.hx

--- source/funkin/import.hx
+++ source/funkin/import.hx
@@ -5,7 +5,8 @@ import flixel.util.FlxDestroyUtil;
 
 import extensions.flixel.FlxCameraEx;
 import extensions.flixel.FlxSoundEx;
-
+import mobile.flixel.FlxVirtualPad;
+import mobile.flixel.input.FlxMobileInputID;
 import funkin.scripting.ScriptConstants;
 import funkin.audio.FunkinSound;
 import funkin.backend.Logger;
