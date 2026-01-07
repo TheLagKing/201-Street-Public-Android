dabea33e5570459713ddcad5ebe3fffd7d9a2f34
CybrrPortGames <dalucanogheaven12@gmail.com>
2026-01-07 23:13:42 +0800

    Update GameOverSubstate.hx

--- source/funkin/states/substates/GameOverSubstate.hx
+++ source/funkin/states/substates/GameOverSubstate.hx
@@ -7,6 +7,7 @@ import flixel.math.FlxPoint;
 import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
 import funkin.backend.MusicBeatSubstate;
+import funkin.backend.MusicBeatState;
 import funkin.states.PlayState;
 import openfl.display.BlendMode;
 
