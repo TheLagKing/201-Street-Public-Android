983fd49c40d21d8389fadd9849143fe95f32fcb2
CybrrPortGames <dalucanogheaven12@gmail.com>
2026-01-07 15:30:28 +0800

    Update BaseOptionsMenu.hx

--- source/funkin/states/options/BaseOptionsMenu.hx
+++ source/funkin/states/options/BaseOptionsMenu.hx
@@ -188,6 +188,9 @@ class BaseOptionsMenu extends MusicBeatSubstate
 
 		if (controls.BACK #if mobile || _virtualpad.buttonB.justPressed #end)
 		{
+			#if mobile
+		removeVirtualPad();
+		#end
 			close();
 			FlxG.sound.play(Paths.sound('cancelMenu'));
 		}
