c743c0fa2d2efc0437953486e746cad5657bb617
CybrrPortGames <dalucanogheaven12@gmail.com>
2026-01-10 11:48:11 +0800

    Update PlayState.hx
    Ok so not that😭

--- source/funkin/states/PlayState.hx
+++ source/funkin/states/PlayState.hx
@@ -2949,7 +2949,7 @@ class PlayState extends MusicBeatState
 		var key:Int = getKeyFromEvent(eventKey);
 		if (cpuControlled || paused || !startedCountdown) return;
 
-		if (key > -1 && (FlxG.keys.checkStatus(eventKey, JUST_PRESSED) || !ClientPrefs.controllerMode))
+		if (key > -1 && (FlxG.keys.checkStatus(eventKey, JUST_PRESSED) || ClientPrefs.controllerMode))
 		{
 			if (!boyfriend.stunned && generatedMusic && !endingSong)
 			{
