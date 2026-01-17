adf12247ce9239967879db7771d448ef7e0183c8
TheLagKing <dalucanogheaven12@gmail.com>
2026-01-18 03:57:04 +0800

    Update StorageUtil.hx

--- source/mobile/StorageUtil.hx
+++ source/mobile/StorageUtil.hx
@@ -36,7 +36,7 @@ class StorageUtil
 			&& !AndroidPermissions.getGrantedPermissions().contains('android.permission.READ_MEDIA_IMAGES'))
 			|| (AndroidVersion.SDK_INT < AndroidVersionCode.TIRAMISU
 				&& !AndroidPermissions.getGrantedPermissions().contains('android.permission.READ_EXTERNAL_STORAGE')))
-			CoolUtil.showMessageBox('Notice!', 'If you accepted the permissions you are all good!' + '\nIf you didn\'t then expect a crash' + '\nPress OK to see what happens :)');
+			CoolUtil.doPopUp('Notice!', 'If you accepted the permissions you are all good!' + '\nIf you didn\'t then expect a crash' + '\nPress OK to see what happens :)');
 
 		try
 		{
@@ -45,7 +45,7 @@ class StorageUtil
 		}
 		catch (e:Dynamic)
 		{
-			CoolUtil.showMessageBox('Error!', 'Please create directory to\n' + StorageUtil.getStorageDirectory() + '\nPress OK to close the game :)');
+			CoolUtil.doPopUp('Error!', 'Please create directory to\n' + StorageUtil.getStorageDirectory() + '\nPress OK to close the game :)');
 			lime.system.System.exit(1);
 		}
 	}
