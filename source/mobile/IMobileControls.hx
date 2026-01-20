c4f55d7fb78fcd7a152f0edca799ce75a732b569
TheLagKing <dalucanogheaven12@gmail.com>
2026-01-20 14:35:53 +0800

    Add files via upload

---
+++ source/mobile/IMobileControls.hx
@@ -0,0 +1,21 @@
+package mobile;
+
+import flixel.util.FlxSignal.FlxTypedSignal;
+import mobile.input.MobileInputManager;
+
+/**
+ * ...
+ * @author: Karim Akra
+ */
+interface IMobileControls
+{
+	public var buttonLeft:TouchButton;
+	public var buttonUp:TouchButton;
+	public var buttonRight:TouchButton;
+	public var buttonDown:TouchButton;
+	public var buttonExtra:TouchButton;
+	public var buttonExtra2:TouchButton;
+	public var instance:MobileInputManager;
+	public var onButtonDown:FlxTypedSignal<TouchButton->Void>;
+	public var onButtonUp:FlxTypedSignal<TouchButton->Void>;
+}
