38eec918166d981c8f3211f7408406a255feb9ce
TheLagKing <dalucanogheaven12@gmail.com>
2026-01-20 14:58:15 +0800

    Update CoolUtil.hx

--- source/funkin/utils/CoolUtil.hx
+++ source/funkin/utils/CoolUtil.hx
@@ -81,6 +81,30 @@ class CoolUtil
 		
 		return daList;
 	}
+
+
+	public static function colorFromString(color:String):FlxColor
+	{
+		var hideChars = ~/[\t\n\r]/;
+		var color:String = hideChars.split(color).join('').trim();
+		if(color.startsWith('0x')) color = color.substring(color.length - 6);
+
+		var colorNum:Null<FlxColor> = FlxColor.fromString(color);
+		if(colorNum == null) colorNum = FlxColor.fromString('#$color');
+		return colorNum != null ? colorNum : FlxColor.WHITE;
+	}
+	public static function listFromString(string:String):Array<String>
+	{
+		var daList:Array<String> = [];
+		daList = string.trim().split('\n');
+		
+		for (i in 0...daList.length)
+		{
+			daList[i] = daList[i].trim();
+		}
+		
+		return daList;
+	}
 	
 	/**
 		Finds the most used Color on a given sprite 
